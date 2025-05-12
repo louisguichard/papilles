51215b6a4799ea22db1b57c432c2c439c8d49df4
louisguichard <jeliraimesmailsunjour@gmail.com>
2025-05-12 17:48:37 +0200

    feat: display category images

--- recipes/models.py
+++ recipes/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField()
+    image = models.ImageField(upload_to="categories/", blank=True, null=True)
 
     def __str__(self):
         return self.name
