cd98e46af0821c45eab5608396df41f7c0ae7e93
louisguichard <jeliraimesmailsunjour@gmail.com>
2025-05-14 11:53:40 +0200

    feat: add Gallery model

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Collection, Recipe
+from .models import Collection, Recipe, Gallery
 
 # Register your models here.
+admin.site.register(Gallery)
 admin.site.register(Collection)
 admin.site.register(Recipe)
--- recipes/models.py
+++ recipes/models.py
@@ -3,11 +3,33 @@ from django.utils.text import slugify
 from django.contrib.auth.models import User
 
 
+class Gallery(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField()
+    image = models.ImageField(upload_to="galleries/", blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
 class Collection(models.Model):
     name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
     description = models.TextField()
     image = models.ImageField(upload_to="collections/", blank=True, null=True)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        related_name="collections",
+        null=True,
+        blank=True,
+    )
 
     def save(self, *args, **kwargs):
         if not self.slug:
@@ -28,10 +50,15 @@ class Recipe(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="recipes", null=True, blank=True
+        User, on_delete=models.SET_NULL, related_name="recipes", null=True, blank=True
     )
     collections = models.ManyToManyField(
         Collection,
+        related_name="recipes",
+        blank=True,
+    )
+    galleries = models.ManyToManyField(
+        Gallery,
         related_name="recipes",
         blank=True,
     )
